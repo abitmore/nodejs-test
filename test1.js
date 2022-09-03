c28fbbc77bfcc7095a1e947838d1636d2e39d40d
Abit <abitmore@users.noreply.github.com>
2022-09-04 00:29:44 +0200

    Update test1.js

--- test1.js
+++ test1.js
@@ -7,3 +7,7 @@ console.log(num);
 var long_num = Long.fromString(""+num);
 console.log(long_num);
 console.log(long_num.toString());
+
+var long_num2 = Long.fromString(""+num,true,0);
+console.log(long_num2);
+console.log(long_num2.toString());
