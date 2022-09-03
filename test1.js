934377161447f910018e87b5f636e31d4042c5bb
Abit <abitmore@users.noreply.github.com>
2022-09-04 00:16:31 +0200

    Update test1.js

--- test1.js
+++ test1.js
@@ -1,2 +1,8 @@
 let Long = require('long');
 console.log("hello world 123");
+
+var num = parseFloat("0.00013")*100000;
+console.log(num);
+
+var long_num = Long.fromString(""+num);
+console.log(long_num);
